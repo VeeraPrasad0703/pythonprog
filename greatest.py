364109c28a94a01fb600cba9623d5f5f2f0fe986
VeeraPrasad0703 <130963141+VeeraPrasad0703@users.noreply.github.com>
2023-04-18 13:01:45 +0530

    Create greatest.py

---
+++ greatest.py
@@ -0,0 +1,12 @@
+n1=int(input("Enter number"))
+n2=int(input("Enter number"))
+n3=int(input("Enter number"))
+if(n1>n2):
+  if(n1>n3):
+    print("greatest is",n1)
+  else:
+    print("greatest is",n3)
+elif(n2>n3):
+  print("greatest is,n2)
+else:
+  print("greatest is",n3)
